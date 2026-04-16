0722ab394018b7705b25b8ac8aec2a82ad5184ab
Vuyisanani <vuyisanani.inbox@gmail.com>
2026-04-16 15:46:55 +0200

    Add files via upload

---
+++ PROB1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace PROB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, i = 0;
+
+            for (i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    sum += i;
+                }
+            }
+            WriteLine(sum);
+        }
+    }
+}
