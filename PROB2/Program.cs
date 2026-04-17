db1e61df894230e4e52df0b822a065484213e091
Vuyisanani <vuyisanani.inbox@gmail.com>
2026-04-17 22:38:44 +0200

    Add files via upload

---
+++ PROB2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace PROB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int first = 1;
+            int second = 2;
+            int sumOfEvens = 0;
+
+            while (first <= 4000000)
+            {
+                if (first % 2 == 0)
+                {
+                    sumOfEvens += first;
+                }
+
+                int next = first + second;
+                first = second;
+                second = next;
+            }
+
+            WriteLine($"The sum of even-valued terms is: {sumOfEvens}");
+        }
+    }
+}
